998ed8919f10ea0af9d0497b684f37b76de2eefd
Solomon Kahsai <abrahams17@univie.ac.at>
2019-09-09 14:18:05 +0200

    call back added to fetchproducts, model refactored to use one getproducts for fetch and for save, and product list renamed from shop put into own folder shop and also add-product put into its own folder admin

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product = require('../models/product')
 
 exports.getAppProduct = (req, res, next) => {
     // res.sendFile( path.join(rootDir, 'views', 'add-product.html') )
-    res.render('add-product',
+    res.render('admin/add-product',
         {
             docTitle: 'add-products',
             formscss: true,
@@ -20,16 +20,17 @@ exports.postAddProduct = (req, res, next) => {
 }
 
 exports.getProdcuts = (req, res, next) => {
-   const products = Product.fetchAllProducts();
-    res.render('shop',
-        {
-            prods: products, 
-            docTitle: 'shop',
-            hasproducts: products.length > 0,
-            activeshop: true,
-            productcss: true,
-            path: '/'
-            
-        })
+   Product.fetchAllProducts((products)=> {
+       res.render('shop/product-list',
+           {
+               prods: products, 
+               docTitle: 'shop',
+               hasproducts: products.length > 0,
+               activeshop: true,
+               productcss: true,
+               path: '/'
+               
+           })
+   });
 }
 
--- models/product.js
+++ models/product.js
@@ -1,15 +1,36 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
+const rootDir = require('../util/path')
+const p = path.join(rootDir, 'data', 'products.json')
 
+const getProductsFromFile = (cb) => {
+  
+    fs.readFile(p, (e, d) => {
+        if (e) {
+            cb([])
+        } else {
+            cb(JSON.parse(d))
+        }
+    })
+}
 module.exports = class Product {
     constructor(title) {
         this.title = title
     }
 
     save() {
-        products.push(this)
+        getProductsFromFile((products) => {
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), e => {
+                console.log(e);
+            })
+
+        })
+       
     }
 
-    static fetchAllProducts() {
-        return products;
+    static fetchAllProducts(cb) {
+        getProductsFromFile(cb)
+
     }
 }
