f0d31f80a136eaeb9227e399bcb0b3ad02240983
Solomon Kahsai <abrahams17@univie.ac.at>
2019-09-09 17:18:16 +0200

    added orders and more fields to the input

--- models/product.js
+++ models/product.js
@@ -1,36 +1,40 @@
-const fs = require('fs')
-const path = require('path')
-const rootDir = require('../util/path')
-const p = path.join(rootDir, 'data', 'products.json')
+const fs = require('fs');
+const path = require('path');
 
-const getProductsFromFile = (cb) => {
-  
-    fs.readFile(p, (e, d) => {
-        if (e) {
-            cb([])
-        } else {
-            cb(JSON.parse(d))
-        }
-    })
-}
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
-    constructor(title) {
-        this.title = title
-    }
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    save() {
-        getProductsFromFile((products) => {
-            products.push(this)
-            fs.writeFile(p, JSON.stringify(products), e => {
-                console.log(e);
-            })
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-        })
-       
-    }
-
-    static fetchAllProducts(cb) {
-        getProductsFromFile(cb)
-
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
