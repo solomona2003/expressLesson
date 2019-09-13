2c563b9a77206c433266d7ea27419fa398a47356
Solomon Kahsai <abrahams17@univie.ac.at>
2019-09-13 21:30:36 +0200

    end of sequelize

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+const sequelize = new Sequelize('node-complete', 'root', 'password', {
   dialect: 'mysql',
   host: 'localhost'
 });
